6d4591d213803263dcdd60a07af58def1842acf7
daniel-dieguez <danguez2001@gmail.com^C>
2024-06-09 19:28:32 -0600

    Se agregaron cambios a las funciones de agregar, eliminar, down y up de la lista

--- src/task/List.jsx
+++ src/task/List.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import style from '../css/index.css'
 
 export default function List() {
 
@@ -14,17 +15,39 @@ const handleInput = (e) =>{
 
 const addTask = ()=>{
 
+    if(newTask.trim() !== ""){ 
+        SetTask( t=> [...t, newTask]);
+        setNewTask("");
+    }
+    alert("debe de ingresar dato");
+    console.log("debe de ingresar dato");
+
 }
 
 const deleteTask = (index) => {
 
+    const updateTask = task.filter((_, i) => i !== index);
+    SetTask(updateTask);
+
 }
 
 const moveTaskUp = (index)=>{
+    if(index > 0){
+        const updateTastk = [...task];
+        [updateTastk[index], updateTastk[index - 1]] = 
+        [updateTastk[index - 1], updateTastk[index]];
+        SetTask(updateTastk);
+    }
 
 }
 
 const moveTaskDown = (index)=>{
+    if(index < task.length - 1 ){
+        const updateTastk = [...task];
+        [updateTastk[index], updateTastk[index + 1]] = 
+        [updateTastk[index + 1], updateTastk[index]];
+        SetTask(updateTastk);
+    }
 
 }
 
@@ -43,16 +66,16 @@ const moveTaskDown = (index)=>{
         </div>
 
         <ol>
-            {task.map((tasks, index) =>
+            {task.map((task, index) =>
                 <li key={index}>
                     <span className='texto'>{task}</span>
                     <button
                     className='eliminar-button'
-                    onClick={() => deleteTask(index)}>Eliminr</button>
+                    onClick={() => deleteTask(index)}>Eliminar</button>
                     <button className='subirTarea'
                     onClick={() => moveTaskUp(index)}>subir</button>
                     <button className='bajarTarea'
-                    onClick={() => moveTaskDown(index)}>subir</button>
+                    onClick={() => moveTaskDown(index)}>bajar</button>
                 </li>
 
             )}
